3d20d18627b7b4c1b340717eb33ab886b68c89ac
Travis Hoffman <traviszhoffman@gmail.com>
2022-04-02 19:12:47 -0600

    First commit

---
+++ cmake-build-debug/CMakeFiles/Coding_Challenge_OIT_4_2_2022.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coding_Challenge_OIT_4_2_2022.dir/main.cpp.obj"
+  "Coding_Challenge_OIT_4_2_2022.exe"
+  "Coding_Challenge_OIT_4_2_2022.exe.manifest"
+  "Coding_Challenge_OIT_4_2_2022.pdb"
+  "libCoding_Challenge_OIT_4_2_2022.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coding_Challenge_OIT_4_2_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Coding_Challenge_OIT_4_2_2022.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Coding_Challenge_OIT_4_2_2022.
+# This may be replaced when dependencies are built.
